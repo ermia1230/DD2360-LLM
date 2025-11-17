5e2022a8cd242d43f6a0faf166dbc6a070b656bc
Gabriel Batista <95219553+GabrielCostaBatista@users.noreply.github.com>
2025-11-17 12:14:52 +0100

    Small bug fix

--- train_gpt2_fp32.cu
+++ train_gpt2_fp32.cu
@@ -663,7 +663,7 @@ void attention_backward(float* dinp, float* dqkvr, float* dpreatt, float* datt, 
                         const float* dout,
                         const float* qkvr, const float* att,
                         int B, int T, int C, int NH) {
-    const int block_size = 256;
+    int block_size = 256;
     int HS = C / NH; // head size
     const float one = 1.0f;
     const float zero = 0.0f; // note beta = 1.0f so that we accumulate gradients (+=)
@@ -685,7 +685,7 @@ void attention_backward(float* dinp, float* dqkvr, float* dpreatt, float* datt, 
     // backward into dv
     cublasCheck(cublasSgemmStridedBatched(cublas_handle, CUBLAS_OP_N, CUBLAS_OP_T, HS, T, T, &one, scratch, HS, T * HS, att, T, T * T, &zero, dv, HS, T * HS, B * NH));
     // backward into preatt
-    const int num_blocks = CEIL_DIV(T, block_size);
+    num_blocks = CEIL_DIV(T, block_size);
     softmax_autoregressive_backward_kernel1<<<dim3(num_blocks, 1), block_size>>>(dpreatt, datt, att, B, T, C, NH);
     cudaCheck(cudaGetLastError());
     // backward into q
