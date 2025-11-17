651b1eb43c74db207a61c6e266115df15c9e2a60
Gabriel Batista <95219553+GabrielCostaBatista@users.noreply.github.com>
2025-11-17 11:01:53 +0100

    Changed kernel layernorm_forward_kernel1 to naive version

--- train_gpt2_fp32.cu
+++ train_gpt2_fp32.cu
@@ -111,50 +111,41 @@ __global__ void encoder_backward_kernel(float* dwte, float* dwpe,
     }
 }
 
-__global__ void layernorm_forward_kernel3(float* __restrict__ out, float* __restrict__ mean, float* __restrict__ rstd,
-                                    const float*  __restrict__ inp, const float*  __restrict__ weight,
-                                    const float* __restrict__ bias, int N, int C) {
-    cg::thread_block block = cg::this_thread_block();
-    cg::thread_block_tile<32> warp = cg::tiled_partition<32>(block);
-    int idx = blockIdx.x * warp.meta_group_size() + warp.meta_group_rank();
-    if(idx >= N) {
-        return;
-    }
+// naive drag and drop implementation into kernel, parallelize over B,T, loop over C
+__global__ void layernorm_forward_kernel1(float* out, float* mean, float* rstd,
+                                 const float* inp, const float* weight, const float* bias,
+                                 int N, int C) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    float eps = 1e-5f;
 
-    // the row of input that this group of threads is responsible for
-    const float* x = inp + idx * C;
-
-    // mean
-    float sum = 0.0f;
-    for (int i = warp.thread_rank(); i < C; i += warp.size()) {
-        sum += x[i];
-    }
-    sum = cg::reduce(warp, sum, cg::plus<float>{});
-    float m = sum / C;
-    if(warp.thread_rank() == 0 && mean != nullptr) {
-        __stcs(mean + idx, m);
-    }
-
-    // rstd
-    sum = 0.0f;
-    for (int i = warp.thread_rank(); i < C; i += warp.size()) {
-        float diff = x[i] - m;
-        sum += diff * diff;
-    }
-    sum = cg::reduce(warp, sum, cg::plus<float>{});
-    float s = rsqrtf(sum / C + 1e-5f);
-    if(warp.thread_rank() == 0 && rstd != nullptr) {
-        __stcs(rstd + idx, s);
-    }
-
-    // final normalization and scaling by weight/bias
-    float* o = out + idx * C;
-    for (int c = warp.thread_rank(); c < C; c += warp.size()) {
-        // load and store using the .cs "streaming" hint to the compiler,
-        // indicating that this data will not be reused soon, and can be streamed through the caches
-        // this allows the threads to get more cache-hits for the (shared) weight and bias parameters
-        float n = s * (__ldcs(x+c) - m);
-        __stcs(o+c, n * weight[c] + bias[c]);
+    if (idx < N) {
+        // seek to the input position inp[idx,:]
+        const float* x = inp + idx * C;
+        // calculate the mean
+        float m = 0.0f;
+        for (int i = 0; i < C; i++) {
+            m += x[i];
+        }
+        m = m / C;
+        // calculate the variance (without any bias correction)
+        float v = 0.0f;
+        for (int i = 0; i < C; i++) {
+            float xshift = x[i] - m;
+            v += xshift * xshift;
+        }
+        v = v / C;
+        // calculate the rstd
+        float s = 1.0f / sqrtf(v + eps);
+        // seek to the output position in out[idx,:]
+        float* out_idx = out + idx * C;
+        for (int i = 0; i < C; i++) {
+            float n = (s * (x[i] - m)); // normalized output
+            float o = n * weight[i] + bias[i]; // scale and shift it
+            out_idx[i] = o; // write
+        }
+        // cache the mean and rstd for the backward pass later
+        mean[idx] = m;
+        rstd[idx] = s;
     }
 }
 
@@ -713,8 +704,8 @@ void layernorm_forward(float* out, float* mean, float* rstd,
                        int B, int T, int C) {
     const int block_size = 512;
     const int N = B * T;
-    const int grid_size = CEIL_DIV(N * 32, block_size);
-    layernorm_forward_kernel3<<<grid_size, block_size>>>(out, mean, rstd, inp, weight, bias, N, C);
+    const int grid_size = CEIL_DIV(N, block_size);
+    layernorm_forward_kernel1<<<grid_size, block_size>>>(out, mean, rstd, inp, weight, bias, N, C);
     cudaCheck(cudaGetLastError());
 }
 
