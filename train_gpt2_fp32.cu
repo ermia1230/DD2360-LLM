6675f60834e398ebda32c294492d1792e1e0a7bc
Gabriel Batista <95219553+GabrielCostaBatista@users.noreply.github.com>
2025-11-17 12:01:02 +0100

    Changed adamw_kernel kernel to naive version

--- train_gpt2_fp32.cu
+++ train_gpt2_fp32.cu
@@ -408,28 +408,18 @@ __global__ void softmax_autoregressive_backward_kernel1(float* dpreatt, const fl
     }
 }
 
-// Implements linear interpolation using only two floating-point operations (as opposed to three in a naive implementation).
-// Reference: https://developer.nvidia.com/blog/lerp-faster-cuda
-__device__ inline float lerp(float start, float end, float weight) {
-    return fma(weight, end, fma(-weight, start, start));
-}
-
-__global__ void adamw_kernel2(float* params_memory, float* grads_memory, float* m_memory, float* v_memory, long num_parameters,
+// naive fused kernel
+__global__ void adamw_kernel1(float* params_memory, const float* grads_memory, float* m_memory, float* v_memory, long num_parameters,
                               float learning_rate, float beta1, float beta2, float beta1_correction, float beta2_correction, float eps, float weight_decay) {
    int i = blockIdx.x * blockDim.x + threadIdx.x;
    if (i >= num_parameters) return;  // guard
-   float grad = grads_memory[i];
-   float m = m_memory[i];
-   float v = v_memory[i];
    // update the first moment (momentum)
-   m = lerp(grad, m, beta1);
-   m_memory[i] = m;
+   m_memory[i] = beta1 * m_memory[i] + (1.0f - beta1) * grads_memory[i];
    // update the second moment (RMSprop)
-   v = lerp(grad * grad, v, beta2);
-   v_memory[i] = v;
-   m /= beta1_correction;  // m_hat
-   v /= beta2_correction;  // v_hat
-   params_memory[i] -= learning_rate * (m / (sqrtf(v) + eps) + weight_decay * params_memory[i]);
+   v_memory[i] = beta2 * v_memory[i] + (1.0f - beta2) * grads_memory[i] * grads_memory[i];
+   float m_hat = m_memory[i] / beta1_correction;
+   float v_hat = v_memory[i] / beta2_correction;
+   params_memory[i] -= learning_rate * (m_hat / (sqrtf(v_hat) + eps) + weight_decay * params_memory[i]);
 }
 
 struct SoftmaxParams {
@@ -1375,7 +1365,7 @@ void gpt2_update(GPT2 *model, float learning_rate, float beta1, float beta2, flo
     int num_blocks = CEIL_DIV(model->num_parameters, block_size);
     float beta1_correction = 1.0f - powf(beta1, t);
     float beta2_correction = 1.0f - powf(beta2, t);
-    adamw_kernel2<<<num_blocks, block_size>>>(model->params_memory, model->grads_memory, model->m_memory, model->v_memory,
+    adamw_kernel1<<<num_blocks, block_size>>>(model->params_memory, model->grads_memory, model->m_memory, model->v_memory,
                                               model->num_parameters,
                                               learning_rate, beta1, beta2, beta1_correction, beta2_correction, eps, weight_decay);
     cudaCheck(cudaGetLastError());
